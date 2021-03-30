2784997e8bdfa39722db53c7993766d575f57eae
龚岳卿 <1727728017@qq.com>
2021-03-30 17:39:11 +0800

    实现webpack

--- 1.webpack/src/index.js
+++ 1.webpack/src/index.js
@@ -1,11 +1,11 @@
 /*
  * @Author: Quinn
  * @Date: 2021-03-30 16:17:12
- * @LastEditTime: 2021-03-30 16:21:27
+ * @LastEditTime: 2021-03-30 16:42:34
  * @LastEditors: quinn
  * @Description:  
  */
 
 //index.js
-import message from './message';
+import message from './message.js';
 console.log(message)
